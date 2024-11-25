daab2617e8893fc44263e25325a5e61ef8a80982
Leonardo Mazzeo <leomazzeo@gmail.com>
2024-11-25 11:16:50 -0300

    getting personal

--- script/scripts.js
+++ script/scripts.js
@@ -2,7 +2,62 @@
 window.onscroll = function() {scrollFunction()};
         
 function scrollFunction() {
-  if (document.body.scrollTop > 8 || document.documentElement.scrollTop > 8) {
+  //if ((document.body.scrollTop > 8 && document.body.scrollTop <= 16) || (document.documentElement.scrollTop > 8 && document.documentElement.scrollTop <= 16)) 
+  //{
+  //  document.getElementById("navbar").style.paddingTop = "0.5vh ";
+  //  document.getElementById("navbar").style.paddingBottom = "0.5vh";
+  //  document.getElementById("navbar").style.height = "5vh";
+  //  document.getElementById("navbar").style.transition = "1.5s";
+  //  document.getElementById("logo").style.fontSize = "3vh";
+  //  document.getElementById("cont2").style.color = "lightgrey";
+  //  document.getElementById("cont2").style.transition = "3s";
+  //  document.getElementById("cont2").style.marginTop = "10vh";
+  //  
+  //} 
+
+  if ((document.body.scrollTop > 8) || (document.documentElement.scrollTop > 8)) 
+  { 
+        
+
+        if ((document.body.scrollTop > 500) || (document.documentElement.scrollTop > 500))
+        {
+            document.getElementById("cont2").style.color = "#3E3E3E";
+            document.getElementById("cont2").style.transition = "1s";
+            document.getElementById("cont2").style.marginTop = "10vh";
+            document.getElementById("navbar").style.paddingTop = "0.5vh ";
+            document.getElementById("navbar").style.paddingBottom = "0.5vh";
+            document.getElementById("navbar").style.height = "5vh";
+            document.getElementById("navbar").style.transition = "1s";
+            document.getElementById("logo").style.fontSize = "3vh";
+        }
+        else 
+        {
+    
+            document.getElementById("cont2").style.color = "lightgrey";
+            document.getElementById("cont2").style.transition = "4s";
+            document.getElementById("cont2").style.marginTop = "10vh"; 
+            document.getElementById("navbar").style.paddingTop = "0.5vh ";
+            document.getElementById("navbar").style.paddingBottom = "0.5vh";
+            document.getElementById("navbar").style.height = "5vh";
+            document.getElementById("navbar").style.transition = "1s";
+            document.getElementById("logo").style.fontSize = "3vh"; 
+        }
+        
+  }
+  //else if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100)
+  //{
+  //  //document.getElementById("navbar").style.paddingTop = "0.5vh ";
+  //  //document.getElementById("navbar").style.paddingBottom = "0.5vh";
+  //  //document.getElementById("navbar").style.height = "5vh";
+  //  //document.getElementById("navbar").style.transition = "1.5s";
+  //  //document.getElementById("logo").style.fontSize = "3vh";
+  //  document.getElementById("cont2").style.color = "#3E3E3E";
+  //  document.getElementById("cont2").style.transition = "3s";
+  //  document.getElementById("cont2").style.marginTop = "40vh";
+  //}
+/*
+  if ((document.body.scrollTop > 8 ) || (document.documentElement.scrollTop > 8 )) 
+  {
     document.getElementById("navbar").style.paddingTop = "0.5vh ";
     document.getElementById("navbar").style.paddingBottom = "0.5vh";
     document.getElementById("navbar").style.height = "5vh";
@@ -12,14 +67,18 @@ function scrollFunction() {
     document.getElementById("cont2").style.transition = "3s";
     document.getElementById("cont2").style.marginTop = "10vh";
     
-  } else {
+  } 
+  */
+  else {
     document.getElementById("navbar").style.paddingTop = "40vh ";
     //document.getElementById("navbar").style.paddingBottom = "30em ";
     document.getElementById("navbar").style.height = "100vh"; 
     document.getElementById("logo").style.fontSize = "10vh";
     document.getElementById("cont2").style.color = "#3E3E3E";
-    document.getElementById("cont2").style.transition = "1s";
+    document.getElementById("cont2").style.transition = "3s";
     document.getElementById("cont2").style.marginTop = "40vh"; 
   }
 }
+
+
 
